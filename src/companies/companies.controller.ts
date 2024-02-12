20987947ba29bfc230d0a77f4ae0c91d7a624441
gabbrieu <gabrielhmendes@hotmail.com>
2024-02-12 00:23:43 -0300

    docs: improve company docs on swagger

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { ApiExtraModels, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Company } from './entities/companies.entity';
 import { FindAllCompaniesUseCase } from './use-cases/find-all-companies.use-case';
 import { FindOneCompanyUseCase } from './use-cases/find-one-company.use-case';
 
 @Controller('companies')
+@ApiTags('Companies')
+@ApiExtraModels(Company)
 export class CompaniesController {
     constructor(
         @Inject(FindAllCompaniesUseCase)
@@ -14,11 +17,23 @@ export class CompaniesController {
     ) {}
 
     @Get()
+    @ApiResponse({
+        status: 200,
+        description: 'Existent companies',
+    })
     async findAll(): Promise<Company[]> {
         return this.findAllCompaniesUseCase.execute();
     }
 
     @Get(':company_id')
+    @ApiResponse({
+        status: 200,
+        description: 'Company found by id',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Company not found by id',
+    })
     async findOne(@Param('company_id') id: string): Promise<Company> {
         return this.findOneCompanyUseCase.execute(id);
     }
--- src/companies/entities/companies.entity.ts
+++ src/companies/entities/companies.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
     Column,
     CreateDateColumn,
@@ -9,20 +10,43 @@ import {
 @Entity('companies')
 export class Company {
     @PrimaryGeneratedColumn('uuid')
+    @ApiProperty({
+        type: String,
+        format: 'uuid',
+        example: '94f9b58d-5e9d-46a4-8d3b-2a0327f4c889',
+        description: 'UUID of a company',
+    })
     id: string;
 
     @Column({ type: 'text', unique: true })
+    @ApiProperty({
+        type: String,
+        example: 'ABC Corp',
+        description: `Company's name`,
+    })
     name: string;
 
     @CreateDateColumn({
         type: 'timestamp with time zone',
         default: () => 'now()',
     })
+    @ApiProperty({
+        type: String,
+        example: '2024-02-12T02:55:32.443Z',
+        description: `Date of company creation`,
+        default: 'now()',
+    })
     created_at: string;
 
     @UpdateDateColumn({
         type: 'timestamp with time zone',
         default: () => 'now()',
+    })
+    @ApiProperty({
+        type: String,
+        example: '2024-02-12T02:55:32.443Z',
+        description: `Date of company update`,
+        default: 'now()',
     })
     updated_at: string;
 }
