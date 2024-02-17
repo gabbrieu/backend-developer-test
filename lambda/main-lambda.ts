b89476183977764e6fdf9994c6bc24862a482347
gabbrieu <gabrielhmendes@hotmail.com>
2024-02-16 21:42:04 -0300

    feat: add global error filter to lambda

--- lambda/main-lambda.ts
+++ lambda/main-lambda.ts
@@ -1,12 +1,16 @@
 import { configure as serverlessExpress } from '@codegenie/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { Callback, Context, Handler } from 'aws-lambda';
+import { HttpExceptionFilter } from '../src/filters/http-exception.filter';
 import { AppLambdaModule } from './app-lambda.module';
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
     const app = await NestFactory.create(AppLambdaModule);
+
+    app.useGlobalFilters(new HttpExceptionFilter());
+
     await app.init();
 
     const expressApp = app.getHttpAdapter().getInstance();
