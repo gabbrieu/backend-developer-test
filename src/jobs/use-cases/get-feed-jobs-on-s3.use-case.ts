3a46e2b0397b627ff53538242e1b4a1e3f923bcd
gabbrieu <gabrielhmendes@hotmail.com>
2024-02-16 21:11:58 -0300

    feat: add logs to get feed jobs on s3 usecase

--- src/jobs/use-cases/get-feed-jobs-on-s3.use-case.ts
+++ src/jobs/use-cases/get-feed-jobs-on-s3.use-case.ts
@@ -1,15 +1,23 @@
 import { GetObjectCommand, GetObjectCommandOutput } from '@aws-sdk/client-s3';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+    Injectable,
+    InternalServerErrorException,
+    Logger,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { s3Client } from '../../utils/aws';
 import { FeedJobDTO } from '../dto/feed-job.dto';
 
 @Injectable()
 export class GetFeedJobsOnS3UseCase {
+    private readonly logger = new Logger(GetFeedJobsOnS3UseCase.name);
+
     constructor(private readonly configService: ConfigService) {}
 
     async execute(): Promise<FeedJobDTO[]> {
         try {
+            this.logger.log('Getting job file from S3');
+
             const bucketName: string = this.configService.getOrThrow<string>(
                 'AWS_S3_FEED_BUCKET_NAME'
             );
@@ -28,6 +36,8 @@ export class GetFeedJobsOnS3UseCase {
             const jsonData: FeedJobDTO[] = JSON.parse(
                 await bucketData.Body.transformToString('utf-8')
             );
+
+            this.logger.log('File retrieved from S3');
 
             return jsonData;
         } catch (error) {
